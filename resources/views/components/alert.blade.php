bbeea52a7e34e67a3c3fe7af1143600578cc9b53
Edgar Montes <mogoedgar06@gmail.com>
2026-03-17 16:00:51 -0600

    Agregacion del component alert

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,10 @@
+<div class="p-4 mb-4 text-sm text-fg-brand-strong rounded-base bg-brand-softer" role="alert">
+  <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+</div>
+<!-- para mandar a llamarlo
+<x-alert/>
+ 
+<x-user-profile/> -->
+<!-- otro ejemplo para que tenga una alerta personalizada 
+<x-alert type="error" :message="$message"/>
+-->  
